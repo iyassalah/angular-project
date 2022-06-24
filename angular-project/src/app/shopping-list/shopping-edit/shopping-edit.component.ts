7cc2306f8d1161bf714d7f95850c7500912a2018
iyassalah <iyasgamer01.p@gmail.com>
2022-06-24 15:55:44 +0300

    start in edit recipe form

--- angular-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angular-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { toJSDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-calendar';
 import { Subscription } from 'rxjs';
 import { ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shoping-list.service';
@@ -9,7 +10,7 @@ import { ShoppingListService } from '../shoping-list.service';
   styleUrls: ['./shopping-edit.component.scss'],
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
-  @ViewChild('f', {static: false}) shopingListForm: NgForm;
+  @ViewChild('f', { static: false }) shopingListForm: NgForm;
   constructor(private shoppingListService: ShoppingListService) {}
   ngOnDestroy(): void {
     throw new Error('Method not implemented.');
@@ -27,20 +28,35 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         this.editedItem = this.shoppingListService.getIngredient(index);
         this.shopingListForm.setValue({
           name: this.editedItem.name,
-          amount: this.editedItem.amount
-        })
+          amount: this.editedItem.amount,
+        });
       }
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const newIngredient = new ingredient(form.value.name, form.value.amount);
     if (this.editMode) {
-      this.shoppingListService.updateIngredient(this.editItemIndex, newIngredient);
+      this.shoppingListService.updateIngredient(
+        this.editItemIndex,
+        newIngredient
+      );
     } else {
       this.shoppingListService.addIngredient(newIngredient);
     }
-  }A
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear() {
+    this.shopingListForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editItemIndex);
+    this.onClear();
+  }
 
   OnDestroy() {
     this.subscription.unsubscribe();
